04a7e3dff55ddd5d441859db25462962dc654f2a
Sanna BS Jammeh <sannabsjammeh54@gmail.com>
2024-11-17 07:06:54 -0800

    commit

--- models/Booking.js
+++ models/Booking.js
@@ -1,42 +1,71 @@
-const mongoose = require('mongoose')
-const { type } = require('os')
+const mongoose = require('mongoose');
 
-const  BookingSchema = new mongoose.Schema ( {
-    fullname: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    company: {
-        type: String,
-        required: true
-    },
-    position: {
-        type: String,
-        required: true
-    },
-    service: {
-        type: String,
-        required: true,
-        enum: ['Digital Solutions', 'Information Technology', 'Blockchain & Engineering']
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    appointmentType: {
-        type: String,
-        required: true,
-        enum: ['Option 1', 'Option 2']
-    },
-    approved: {
-        type: Boolean,
-        default: false
-    }
-})
+const bookingSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  position: {
+    type: String,
+    required: true,
+  },
+  service: {
+    type: String,
+    enum: ['Digital Solutions', 'Information Technology', 'Blockchain & Engineering'],
+    required: true,
+  },
+  digitalService: {
+    type: String,
+    enum: [
+      'webDevelopment',
+      'eCommerce',
+      'mobileApplications',
+      'digitalMarketing',
+      'contentManagement',
+      'selfServiceSystems',
+    ],
+  },
+  itService: {
+    type: String,
+    enum: [
+      'aiBigData',
+      'customSoftware',
+      'cloudCybersecurity',
+      'businessAutomation',
+      'databaseNetwork',
+      'digitalForensics',
+    ],
+  },
+  engineeringService: {
+    type: String,
+    enum: [
+      'bitcoinMining',
+      'roboticsCommunication',
+      'biomedicalEngineering',
+      'agroTechnologies',
+    ],
+  },
+  appointmentType: {
+    type: String,
+    enum: ['Virtual', 'In-person'],
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const Booking = mongoose.model('Booking', BookingSchema)
-module.exports = Booking
+module.exports = mongoose.model('Booking', bookingSchema);
